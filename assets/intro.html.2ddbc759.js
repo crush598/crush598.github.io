4115aa6901b54e908439f043255bd7242cc4f0ca
Hush <crush@tju.edu.cn>
2022-11-15 18:06:21 +0800

    2022-11-15-18:06:21

---
+++ assets/intro.html.2ddbc759.js
@@ -0,0 +1,4 @@
+import{_ as n}from"./_plugin-vue_export-helper.cdc0426e.js";import{o,c as r,a as e,b as t}from"./app.bb49c734.js";const a={},c=e("h2",{id:"🐼me",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#🐼me","aria-hidden":"true"},"#"),t(" 🐼Me")],-1),s=e("pre",null,[e("code",null,`- TJU AI 本科 
+`)],-1),d=e("h3",{id:"技能",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#技能","aria-hidden":"true"},"#"),t(" 技能")],-1),_=e("pre",null,[e("code",null,`- Python/C++
+- 天高不见心，风静草虫吟。
+`)],-1),i=[c,s,d,_];function h(l,u){return o(),r("div",null,i)}const p=n(a,[["render",h],["__file","intro.html.vue"]]);export{p as default};
